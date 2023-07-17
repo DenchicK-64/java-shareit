4f2e3b4146a00234fb0ada347c20379af2c01a85
Denis S <richardthell64@gmail.com>
2023-07-17 11:59:35 +0400

    ТЗ 15. Исправления после первого ревью.

--- src/test/java/ru/practicum/shareit/request/ItemRequestMapperTest.java
+++ src/test/java/ru/practicum/shareit/request/ItemRequestMapperTest.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.request;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.dto.ItemRequestDtoWithItems;
@@ -9,23 +10,44 @@ import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ItemRequestMapperTest {
+    @Mock
+    private Clock clock;
+    private static final ZonedDateTime NOW_ZDT = ZonedDateTime.of(
+            2023,
+            7,
+            10,
+            10,
+            10,
+            10,
+            0,
+            ZoneId.of("UTC")
+    );
+    private LocalDateTime now = LocalDateTime.of(
+            2023,
+            7,
+            10,
+            10,
+            10,
+            10);
     User testUser = new User(1L, "Test_User", "mail@somemail.ru");
     UserDto testUserDto = new UserDto(1L, "Test_User", "mail@somemail.ru");
 
-    ItemRequest itemRequest = new ItemRequest(1L, "Test_Description", testUser,
-            LocalDateTime.now().plusHours(3));
+    ItemRequest itemRequest = new ItemRequest(1L, "Test_Description", testUser, now);
 
-    ItemRequestDto itemRequestDto = new ItemRequestDto(1L, "Test_Description", testUserDto, LocalDateTime.now().plusHours(3));
+    ItemRequestDto itemRequestDto = new ItemRequestDto(1L, "Test_Description", testUserDto, now);
     ItemDto testItemDto = new ItemDto(1L, "Test_Name", "Test_Description", true, null);
 
     ItemRequestDtoWithItems itemRequestDtoWithItems = new ItemRequestDtoWithItems(1L, "Test_Description", testUserDto,
-            LocalDateTime.now().plusHours(3), List.of(testItemDto));
+            now, List.of(testItemDto));
 
     @Test
     public void toItemRequestDtoTest() {
